0a4bec76a2c1aa610bc8bcd035a2fab561073e5a
dayanidi <93707264+dayanid@users.noreply.github.com>
2022-10-26 17:29:14 +0530

    Update tele.py

--- tele.py
+++ tele.py
@@ -15,7 +15,7 @@ def testfunction(update:Update,context:CallbackContext):
 
 
 def test1(update:Update,context:CallbackContext):
-    file = "frame.jpg"
+    file = "file.jpg"
     path = {"photo":open(file,"rb")}
     resp = r.post(f"https://api.telegram.org/bot5584328065:AAHIM8QNc8cU_0ktfj8li9V6TfPXc6xeEt8/sendPhoto?chat_id=-895245832&caption=",files=path)
 
